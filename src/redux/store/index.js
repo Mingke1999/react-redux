2146a9a1cf8be3f9a1d5aac26eb6bbc8c5a7cc57
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-25 23:27:58 +1000

    middleware logger

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,24 @@
 /**
  * create store
  */
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
 
 import rootReducer from "../reducers/index";
+import reduxLogger from "redux-logger"
 import {composeWithDevTools} from "redux-devtools-extension"
+//middleware 1
+/*
+const logger = store =>next =>action =>{
+    console.log("dispatch ->",action)
+    let result = next(action)
+    console.log("next state->",store.getState());
+    return result;
+}
+*/
+
+
 /**
  * createStore(reducer,middleware)
  */
-const store = createStore(rootReducer,composeWithDevTools());
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(reduxLogger)));
 export default store;
